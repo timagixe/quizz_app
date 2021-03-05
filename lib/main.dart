114908f4f23b0ac05ab6dab41f272632907e9a2d
Tima Gixe <timagixe@protonmail.com>
2021-03-05 12:20:58 +0200

    Add Scaffold()

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Button clicked');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('My App'));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Text('Question 1'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 4'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
